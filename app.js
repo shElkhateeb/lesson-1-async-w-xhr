4c6afe4a1ce1d9a9d4b9a47db0eff8029e893092
Shorouk Elkhateeb <shrouca@gmail.com>
2022-01-11 17:26:22 +0200

    feat: Get articles from nyt

--- app.js
+++ app.js
@@ -26,8 +26,29 @@
         
     }
 
+    function addArticles() {
+        const data = JSON.parse(this.responseText);
+        let htmlContent = '';
+
+        //check if articles found
+        if(data.response.docs.length != 0){
+            for (let i = 0; i<data.response.docs.length; i++){
+                htmlContent += `<h2><a href=${data.response.docs[i].web_url} target="_blank">${data.response.docs[i].headline.main}</a></h2>
+                <p>${data.response.docs[i].abstract}</p>`
+            }
+        } else {
+            htmlContent = '<p>Articles not found</p>';
+        }
+        //display articles if found or an error message if not found
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
     function handleImgError() {
         alert('faild getting image!');
+    }
+
+    function handleArticleError() {
+        alert('faild getting article!');
     }
 
     form.addEventListener('submit', function (e) {
@@ -39,10 +60,19 @@
         const unsplashRequest = new XMLHttpRequest();
 
         unsplashRequest.onload = addImage;
-        unsplashRequest.onerror = handleError;
+        unsplashRequest.onerror = handleImgError;
 
         unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
         unsplashRequest.setRequestHeader('Authorization', 'Client-ID eVvhMtgcwefg9uV2yON8YC2l3Pt-aFtgeY18CkpEpFQ');
         unsplashRequest.send();
+
+        // create New York Times request
+        const nytRequest = new XMLHttpRequest();
+
+        nytRequest.onload = addArticles;
+        nytRequest.onerror = handleArticleError;
+
+        nytRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=umb6APL0yIH53kIeGIZxdp8SIT41Rsy3`);
+        nytRequest.send();
     });
 })();
